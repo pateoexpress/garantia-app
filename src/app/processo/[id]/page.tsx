0e7208b9faff3bd766c9ced57e912f2e8c80e247
pateoexpress <158050441+pateoexpress@users.noreply.github.com>
2024-09-03 09:26:25 -0300

    Update page.tsx

--- src/app/processo/[id]/page.tsx
+++ src/app/processo/[id]/page.tsx
@@ -7,21 +7,48 @@ interface ProcessoInfoPageProps {
   };
 }
 
-const ProcessoInfoPage: React.FC<ProcessoInfoPageProps> = async ({ params }) => {
+const ProcessoInfoPage: React.FC<ProcessoInfoPageProps> = async ({
+  params,
+}) => {
   const { id } = params;
 
   const res = await db.erro_consultor.findUnique({ where: { xata_id: id } });
 
-  const localDate = res?.data ? new Date(res.data.toLocaleString('en-US', { timeZone: 'UTC' })) : null;
+  const localDate = res?.data
+    ? new Date(res.data.toLocaleString("en-US", { timeZone: "UTC" }))
+    : null;
 
   return (
-    <div>
-      <h1>{res?.consultor}</h1>
-      <p>{res?.categoria}</p>
-      <p>{localDate?.toLocaleDateString('pt-BR')}</p>
-      <p>{res?.inconsistencias}</p>
-      <p>{res?.orientacao}</p>
-      <p>{res?.os}</p>
+    <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-10">
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">
+        {res?.consultor}
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="bg-gray-100 p-4 rounded-lg">
+          <h2 className="text-lg font-semibold text-gray-700">OS</h2>
+          <p className="text-gray-600">{res?.os}</p>
+        </div>
+        <div className="bg-gray-100 p-4 rounded-lg">
+          <h2 className="text-lg font-semibold text-gray-700">Data</h2>
+          <p className="text-gray-600">
+            {localDate?.toLocaleDateString("pt-BR")}
+          </p>
+        </div>
+        <div className="bg-gray-100 p-4 rounded-lg">
+          <h2 className="text-lg font-semibold text-gray-700">Categoria</h2>
+          <p className="text-gray-600">{res?.categoria}</p>
+        </div>
+        <div className="bg-gray-100 p-4 rounded-lg">
+          <h2 className="text-lg font-semibold text-gray-700">
+            Inconsistência
+          </h2>
+          <p className="text-gray-600">{res?.inconsistencias}</p>
+        </div>
+        <div className="bg-gray-100 p-4 rounded-lg sm:col-span-2">
+          <h2 className="text-lg font-semibold text-gray-700">Orientação</h2>
+          <p className="text-gray-600">{res?.orientacao}</p>
+        </div>
+      </div>
     </div>
   );
 };
